d492d8d4de05bcb61be0c3b503c99bed08466e50
Pavel <pavellapytko@gmail.com>
2020-10-05 21:42:35 +0300

    Initial commit.

---
+++ 10.10/1.py
@@ -0,0 +1,8 @@
+a = int(input("Введите число: "))
+
+if a >= -10:
+	print(a + 5)
+
+elif a > 10:
+	print(a - 10)
+	
---
+++ 10.10/2.py
@@ -0,0 +1,37 @@
+a = int(input("Введите номер месяца:"))
+
+if a == 1:
+	print("Январь")
+elif a == 2:
+	print("Февраль")
+
+if a == 3:
+	print("Март")
+elif a == 4:
+	print("Апрель")
+
+if a == 5:
+	print("Май")
+elif a == 6:
+	print("Июнь")
+
+if a == 7:
+	print("Июль")
+elif a == 8:
+	print("Август")
+
+if a == 9:
+	print("Сентябрь")
+elif a == 10:
+	print("Октябрь")
+
+if a == 11:
+	print("Ноябрь")
+elif a == 12:
+	print("Декабрь")
+
+if a > 12:
+	print("Такого месяца нет!!!!")
+elif a < 1:
+	print("Такого месяца нет!!!!")
+
